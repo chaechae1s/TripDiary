9ea7f7d26685f86b9ddfdc0a095a793568885310
P-chaewon <100437569+chaechae1s@users.noreply.github.com>
2022-09-27 21:11:58 +0900

    Initial commit

---
+++ src/test/java/com/toy/tripdiary/TripDiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toy.tripdiary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripDiaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
